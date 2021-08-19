a93a75cd21bda1919715fb8a7a9d9ff2f13a05ea
SkullCrusher <boxhead.two.play@gmail.com>
2021-08-19 03:17:24 -0700

    CSP-13,CSP-14,CSP15: Terms + privacy styling

--- src/containers/Legal/index.js
+++ src/containers/Legal/index.js
@@ -5,15 +5,21 @@ const legalStuff = {
   "terms": {
     title: "Terms of Service",
     rows: [
-      "terms wip 1",
-      "terms wip 2",
+      "This is just for fun and hopefully some swag so here is just some simple points.",
+      "You grant me the right to send and save whatever you enter on the site.",
+      "You understand that you are using this software at your own risk. Any bugs, exploits, or etc that may happen are your own responsiblity.",
+      "Do not transfer illegal material using this website.",
+      "By using this site you understand and accept that people may transfer material that you find offensive. I do not want anyone to be offended by I do not control any of the material sent to you.",
+      "Do not abuse the system or attempt to disrupt others playing the system.",
     ]
   },
   "privacy": {
     title: "Privacy Policy",
     rows: [
-      "privacy wip 1",
-      "privacy wip 2",
+      "This is just for fun and hopefully some swag so here is just some simple points.",
+      "All of your drawings, captions, and usernames are shared with anyone connected and are stored. If you draw something really good I might even post it in my Discord server.",
+      "The site is hosted on Cloudflare workers and pages so please review the privacy policy for more details on what they store.",
+      "We are using Cloudflare web analytics please view their privacy policy for more details on what they store.",
     ]
   }
 }
